3accb96a4200a3121b3e2c02c92c261ef0924ee0
André Escorel <gustavo.escorel@gmail.com>
2025-11-10 14:06:11 -0300

    refactor(cmd): moved uart cmd parsiong to separated driver.
    - added 3thr part circular buffer lib.
    - moved uart rx_callback to cmd_buffer.c

---
+++ Core/Inc/cmd_driver.h
@@ -0,0 +1,22 @@
+#if !defined(CMD_DRIVER_H)
+#define CMD_DRIVER_H
+
+#include "main.h"
+#include "circular_buffer.h"
+
+#define CMD_BUFFER_SIZE 1024
+
+typedef enum
+{
+  CMD_STATE_IDLE = 0,
+  CMD_STATE_RECEIVING,
+  CMD_STATE_READY,
+} cmd_state_t;
+
+typedef struct cmd_driver cmd_driver_t;
+typedef void (*cmd_ready_callback_t)(char *cmd, uint16_t size);
+
+void cmd_tick(void);
+void cmd_driver_init(UART_HandleTypeDef *huart, cmd_ready_callback_t callback);
+cmd_state_t cmd_get_state();
+#endif // CMD_DRIVER_H
