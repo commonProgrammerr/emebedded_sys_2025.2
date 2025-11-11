d137ca12ccd7f3b726641c267ff5318ca70a901f
André Escorel <gustavo.escorel@gmail.com>
2025-11-11 09:47:51 -0300

    refactor(main.c): changed screen update process to use MAX7219_driver code.

---
+++ Core/Inc/MAX7219_driver.h
@@ -0,0 +1,40 @@
+#ifndef MAX7219_DRIVER_H
+#define MAX7219_DRIVER_H
+
+#include "main.h"
+
+#define MAX7219_REG_NOOP 0x00
+#define MAX7219_REG_DIGIT0 0x01
+#define MAX7219_REG_DIGIT1 0x02
+#define MAX7219_REG_DIGIT2 0x03
+#define MAX7219_REG_DIGIT3 0x04
+#define MAX7219_REG_DIGIT4 0x05
+#define MAX7219_REG_DIGIT5 0x06
+#define MAX7219_REG_DIGIT6 0x07
+#define MAX7219_REG_DIGIT7 0x08
+#define MAX7219_REG_DECODEMODE 0x09
+#define MAX7219_REG_INTENSITY 0x0A
+#define MAX7219_REG_SCANLIMIT 0x0B
+#define MAX7219_REG_SHUTDOWN 0x0C
+#define MAX7219_REG_DISPLAYTEST 0x0F
+
+typedef (*MAX7219_Tx_CpltCallback_t)(SPI_HandleTypeDef *hspi);
+
+typedef struct MAX7219_driver
+{
+    SPI_HandleTypeDef *hspi;
+    GPIO_TypeDef *cs_port;
+    uint16_t cs_pin;
+    uint8_t screen_buffer[8];
+    volatile uint8_t display_line;
+    MAX7219_Tx_CpltCallback_t tx_cplt_callback;
+} MAX7219_driver_t;
+
+
+void MAX7219_Init(SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port, uint16_t cs_pin, MAX7219_Tx_CpltCallback_t tx_cplt_callback);
+void MAX7219_Write(uint16_t address, uint8_t data);
+void MAX7219_TxCpltHandle(SPI_HandleTypeDef *hspi);
+void MAX7219_UpdateScreen(uint8_t new_screen[8]);
+
+static MAX7219_driver_t max7219;
+#endif // MAX7219_DRIVER_H
---
+++ Core/Src/MAX7219_driver.c
@@ -0,0 +1,60 @@
+#include "MAX7219_driver.h"
+
+void MAX7219_Init(SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port, uint16_t cs_pin, MAX7219_Tx_CpltCallback_t tx_cplt_callback)
+{
+    max7219.hspi = hspi;
+    max7219.cs_port = cs_port;
+    max7219.cs_pin = cs_pin;
+    max7219.display_line = 0;
+    max7219.tx_cplt_callback = tx_cplt_callback;
+
+    // Disable display test
+    HAL_SPI_Transmit(hspi, (uint8_t[]){MAX7219_REG_DISPLAYTEST, 0x00}, 2, HAL_MAX_DELAY);
+
+    // Enter shutdown mode to configure
+    HAL_SPI_Transmit(hspi, (uint8_t[]){MAX7219_REG_SHUTDOWN, 0x00}, 2, HAL_MAX_DELAY);
+
+    // Set scan limit to 8 digits
+    HAL_SPI_Transmit(hspi, (uint8_t[]){MAX7219_REG_SCANLIMIT, 0x07}, 2, HAL_MAX_DELAY);
+
+    // Disable decoding (matrix mode)
+    HAL_SPI_Transmit(hspi, (uint8_t[]){MAX7219_REG_DECODEMODE, 0x00}, 2, HAL_MAX_DELAY);
+
+    // Set medium intensity
+    HAL_SPI_Transmit(hspi, (uint8_t[]){MAX7219_REG_INTENSITY, 0x00}, 2, HAL_MAX_DELAY);
+
+    // Clear all digits
+    MAX7219_UpdateScreen((uint8_t[]){0,0,0,0,0,0,0,0});
+
+    // Exit shutdown mode
+    MAX7219_Write(MAX7219_REG_SHUTDOWN, 0x01);
+}
+
+
+void MAX7219_TxCpltHandle(SPI_HandleTypeDef *hspi)
+{
+    if (hspi == max7219.hspi)
+    {
+        // Lower the LOAD/CS pin
+        HAL_GPIO_WritePin(max7219.cs_port, max7219.cs_pin, GPIO_PIN_RESET);
+        // Raise the LOAD/CS pin
+        HAL_GPIO_WritePin(max7219.cs_port, max7219.cs_pin, GPIO_PIN_SET);
+
+        if(max7219.display_line < 8)
+        {
+            uint8_t data[] = {MAX7219_REG_DIGIT0 + max7219.display_line, max7219.screen_buffer[max7219.display_line++]};
+            HAL_SPI_Transmit_DMA(max7219.hspi, data, 2);
+        } 
+        else if (max7219.tx_cplt_callback != NULL) // Call user-defined callback if set
+        {
+            max7219.tx_cplt_callback(hspi);
+        }
+    }
+}
+
+void MAX7219_UpdateScreen(uint8_t new_screen[8])
+{
+    max7219.display_line = 0;
+    memcpy(max7219.screen_buffer, new_screen, 8);
+    HAL_SPI_Transmit_DMA(max7219.hspi, (uint8_t []){MAX7219_REG_DIGIT0, max7219.screen_buffer[max7219.display_line++]}, 2);
+}
