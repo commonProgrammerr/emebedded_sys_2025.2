53900a9a21eb2757ba5b958252ecbdfe40c0f6c8
André Escorel <gustavo.escorel@gmail.com>
2025-11-24 22:40:02 -0300

    refactor: removed drivers files - removed pcf8591 driver files, implemming on easy way - removed unused MAX7219 driver files

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "PCF8591_driver.h"
 #include <math.h>
 /* USER CODE END Includes */
 
@@ -53,10 +52,35 @@ typedef enum
   rx = 1,
   tx = 2,
 } i2c_flag_t;
+
+typedef enum {
+  PCF8591_CHANNEL_A0 = 0,
+  PCF8591_CHANNEL_A1 = 1,
+  PCF8591_CHANNEL_A2 = 2,
+  PCF8591_CHANNEL_A3 = 3,
+} pcf8591_channel_t;
+typedef enum {
+  four_single_ended = 0b00,
+  three_differential = 0b01,
+  single_ended_and_differential_mixed = 0b10,
+  two_differential = 0b11,
+} analog_input_programming_t;
+
+typedef struct pcf8591_config
+{
+  uint8_t :1;
+  uint8_t analog_output_enabled: 1;
+  analog_input_programming_t analog_input_programming: 2;
+  uint8_t :1;
+  uint8_t auto_increment_flag: 1;
+  pcf8591_channel_t adc_selected_channel: 2;
+} pcf8591_config_t;
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define PCF8591_ADDRESS (0x48 << 1) // Shifted PCF8591 I2C address
 #define I2C_INTERFACE hi2c3
 #define I2C_INTERFACE_INSTANCE I2C3
 #define SIN_WAVE_SAMPLES 512
@@ -98,6 +122,14 @@ volatile SystemState last_state = SYSTEM_STATE_1;
 volatile i2c_flag_t i2c = idle;
 volatile uint8_t mode = 1;
 volatile uint8_t read = 0;
+static pcf8591_config_t pcf8591_config = {
+  .analog_output_enabled=1,
+  .analog_input_programming=four_single_ended,
+  .auto_increment_flag=0,
+  .adc_selected_channel=PCF8591_CHANNEL_A0,
+};
+static uint16_t pcf8591_value = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -119,8 +151,8 @@ void next_state();
 void populate_sin_wave_buff(uint16_t wave_amplitude);
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
-void PCF8591_TxCpltCallback(I2C_HandleTypeDef *hi2c);
-void PCF8591_RxCpltCallback(I2C_HandleTypeDef *hi2c);
+HAL_StatusTypeDef PCF8591_set_channel_index(pcf8591_channel_t channel_index);
+HAL_StatusTypeDef PCF8591_read_analog_channel();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -200,9 +232,9 @@ static void set_pwm_duty(TIM_HandleTypeDef *htim, uint32_t channel, uint8_t duty
   __HAL_TIM_SET_COMPARE(htim, channel, pulse);
 }
 
-void PCF8591_TxCpltCallback(I2C_HandleTypeDef *hi2c) { i2c = tx; }
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) { i2c = tx; }
 
-void PCF8591_RxCpltCallback(I2C_HandleTypeDef *hi2c) { i2c = rx; }
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c) { i2c = rx; }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
@@ -252,12 +284,28 @@ void populate_sin_wave_buff(uint16_t wave_amplitude)
     sin_wave_buff[i] = (uint16_t)(v * wave_amplitude);
   }
 }
+
+HAL_StatusTypeDef PCF8591_read_analog_channel()
+{
+  // Read two bytes: first byte is a dummy, second byte is the actual analog value
+  return HAL_I2C_Master_Receive_IT(&I2C_INTERFACE, PCF8591_ADDRESS, (uint8_t *)&pcf8591_value, 2);
+}
+
+HAL_StatusTypeDef PCF8591_set_channel_index(pcf8591_channel_t channel_index)
+{
+  if (channel_index > 3)
+    return HAL_ERROR; // Invalid channel index
+
+  pcf8591_config.adc_selected_channel = channel_index;
+  return HAL_I2C_Master_Transmit_IT(&I2C_INTERFACE, PCF8591_ADDRESS, (uint8_t *)&pcf8591_config, 1);
+}
+
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
@@ -293,7 +341,6 @@ int main(void)
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
   set_system_state(SYSTEM_STATE_1);
-  PCF8591_Init(&I2C_INTERFACE, NULL, PCF8591_TxCpltCallback, PCF8591_RxCpltCallback);
   HAL_TIM_Base_Start_IT(&htim2);            // Start timer for periodic tasks
   HAL_TIM_Base_Start(&htim4);                // Start TIM4 for DAC DMA triggering
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4); // Start PWM for DAC output
@@ -311,7 +358,7 @@ int main(void)
       break;
     case SYSTEM_STATE_2:
       read = 0;
-      PCF8591_set_channel_index(PCF8591_A0);
+      PCF8591_set_channel_index(PCF8591_CHANNEL_A0);
       __WFI(); // Wait for interrupt to save power
       break;
     case SYSTEM_STATE_3:
@@ -319,12 +366,13 @@ int main(void)
       __WFI(); // Wait for interrupt to save power
       break;
     case SYSTEM_STATE_4:
-      set_pwm_duty(&htim1, TIM_CHANNEL_4, pcf8591.analog_data[PCF8591_A0]);
+      pcf8591_value &= 0x00FF; // Ensure we only use the lower 8 bits
+      set_pwm_duty(&htim1, TIM_CHANNEL_4, pcf8591_value);
       i2c = idle;
       break;
     case SYSTEM_STATE_5:
       read = 0;
-      PCF8591_set_channel_index(PCF8591_A1);
+      PCF8591_set_channel_index(PCF8591_CHANNEL_A1);
       __WFI(); // Wait for interrupt to save power
       break;
     case SYSTEM_STATE_6:
@@ -332,7 +380,8 @@ int main(void)
       __WFI(); // Wait for interrupt to save power
       break;
     case SYSTEM_STATE_7:
-      uint16_t new_wave_amplitude = (uint16_t)(pcf8591.analog_data[PCF8591_A1] * SIN_WAVE_MAX_AMPLITUDE / UINT8_MAX);
+      pcf8591_value &= 0x00FF; // Ensure we only use the lower 8 bits
+      uint16_t new_wave_amplitude = (uint16_t)(pcf8591_value * SIN_WAVE_MAX_AMPLITUDE / UINT8_MAX);
       populate_sin_wave_buff(new_wave_amplitude);
       i2c = idle;
       break;
@@ -348,24 +397,24 @@ int main(void)
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-   */
+  */
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -382,7 +431,7 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-   */
+  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -397,10 +446,10 @@ void SystemClock_Config(void)
 }
 
 /**
- * @brief ADC3 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief ADC3 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_ADC3_Init(void)
 {
 
@@ -415,7 +464,7 @@ static void MX_ADC3_Init(void)
   /* USER CODE END ADC3_Init 1 */
 
   /** Common config
-   */
+  */
   hadc3.Instance = ADC3;
   hadc3.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
   hadc3.Init.Resolution = ADC_RESOLUTION_12B;
@@ -437,7 +486,7 @@ static void MX_ADC3_Init(void)
   }
 
   /** Configure Regular Channel
-   */
+  */
   sConfig.Channel = ADC_CHANNEL_15;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
@@ -455,10 +504,10 @@ static void MX_ADC3_Init(void)
 }
 
 /**
- * @brief DAC1 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief DAC1 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_DAC1_Init(void)
 {
 
@@ -473,7 +522,7 @@ static void MX_DAC1_Init(void)
   /* USER CODE END DAC1_Init 1 */
 
   /** DAC Initialization
-   */
+  */
   hdac1.Instance = DAC1;
   if (HAL_DAC_Init(&hdac1) != HAL_OK)
   {
@@ -481,7 +530,7 @@ static void MX_DAC1_Init(void)
   }
 
   /** DAC channel OUT2 config
-   */
+  */
   sConfig.DAC_SampleAndHold = DAC_SAMPLEANDHOLD_DISABLE;
   sConfig.DAC_Trigger = DAC_TRIGGER_T4_TRGO;
   sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
@@ -494,13 +543,14 @@ static void MX_DAC1_Init(void)
   /* USER CODE BEGIN DAC1_Init 2 */
 
   /* USER CODE END DAC1_Init 2 */
+
 }
 
 /**
- * @brief I2C3 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief I2C3 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_I2C3_Init(void)
 {
 
@@ -526,14 +576,14 @@ static void MX_I2C3_Init(void)
   }
 
   /** Configure Analogue filter
-   */
+  */
   if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Digital filter
-   */
+  */
   if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK)
   {
     Error_Handler();
@@ -541,13 +591,14 @@ static void MX_I2C3_Init(void)
   /* USER CODE BEGIN I2C3_Init 2 */
 
   /* USER CODE END I2C3_Init 2 */
+
 }
 
 /**
- * @brief TIM1 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_TIM1_Init(void)
 {
 
@@ -619,13 +670,14 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
+
 }
 
 /**
- * @brief TIM2 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_TIM2_Init(void)
 {
 
@@ -663,13 +715,14 @@ static void MX_TIM2_Init(void)
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
+
 }
 
 /**
- * @brief TIM4 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_TIM4_Init(void)
 {
 
@@ -707,13 +760,14 @@ static void MX_TIM4_Init(void)
   /* USER CODE BEGIN TIM4_Init 2 */
 
   /* USER CODE END TIM4_Init 2 */
+
 }
 
 /**
- * @brief USART2 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -741,11 +795,12 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
 }
 
 /**
- * Enable DMA controller clock
- */
+  * Enable DMA controller clock
+  */
 static void MX_DMA_Init(void)
 {
 
@@ -772,13 +827,14 @@ static void MX_DMA_Init(void)
   /* DMA2_Channel5_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Channel5_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Channel5_IRQn);
+
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -832,9 +888,9 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -847,12 +903,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
