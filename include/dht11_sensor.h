4c315be1fecf423cb1171b1b6d9e2110e932cd20
André Escorel <gustavo.escorel@gmail.com>
2025-12-09 01:02:36 -0300

    refactor sensor_base.h dht11_sensor.h KY-037_sensor.h to improve modularity and maintainability

--- include/dht11_sensor.h
+++ include/dht11_sensor.h
@@ -4,14 +4,13 @@
 #include "sensor_base.h"
 #include "esp32-dht11.h"
 
-typedef struct dht11_sensor
+typedef struct dht11_context
 {
-    sensor_base_t base;
     dht11_t sensor;
     uint32_t timeout;
-} dht11_sensor_t;
+} dht11_context_t;
 
-SensorStatus_t dht11_init(sensor_base_t* self, uint8_t pin, uint32_t timeout);
-SensorStatus_t dht11_read_data(sensor_base_t* self, void* data);
-SensorStatus_t dht11_deinit(sensor_base_t* self);
+SensorStatus_t dht11_init(sensor_base_t *self, uint8_t pin, uint32_t timeout);
+SensorStatus_t dht11_read_data(sensor_base_t *self, void *data);
+SensorStatus_t dht11_deinit(sensor_base_t *self);
 #endif // DHT11_SENSOR_H
