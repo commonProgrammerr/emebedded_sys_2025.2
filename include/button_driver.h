8fae94a9d0b3218b0bcd84618f8916ae4587224f
André Escorel <gustavo.escorel@gmail.com>
2025-12-14 17:47:33 -0300

    fix(button): improve button event detection and debounce logic
    - Refactor button_task to handle long press, normal press, and short press events more reliably
    - Add logic to avoid multiple triggers for long press and debounce on release
    - Ensure button callback is only called for the correct event type
    - Update main.c to handle button events in a loop with visual feedback on WARNING_STATE_GPIO
    - Improve code formatting and comments for clarity

--- include/button_driver.h
+++ include/button_driver.h
@@ -6,7 +6,9 @@
 #include "freertos/task.h"
 
 // Tipos de eventos que o botão pode gerar
-typedef enum {
+typedef enum
+{
+    BUTTON_PRESS_NULL = 0,
     BUTTON_PRESS_SHORT,  // Toque rápido
     BUTTON_PRESS_NORMAL, // Toque normal (ex: entre 500ms e 2s)
     BUTTON_PRESS_LONG    // Toque longo (ex: > 2s)
@@ -17,11 +19,12 @@ typedef enum {
 typedef void (*button_callback_t)(int pin, button_event_t event);
 
 // A "Classe" Botão
-typedef struct {
-    gpio_num_t pin;             // O pino físico
-    button_callback_t callback; // A função que será chamada quando ocorrer evento
+typedef struct
+{
+    gpio_num_t pin;              // O pino físico
+    button_callback_t callback;  // A função que será chamada quando ocorrer evento
     TaskHandle_t task_to_notify; // Task que receberá o notify
-    
+
     // Variáveis internas de controle (privadas na lógica)
     int64_t press_start_time;
     bool is_pressed;
